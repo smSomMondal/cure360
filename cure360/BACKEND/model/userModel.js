395a2543f1ce33360c37a1a2a25e29fb363bcfb8
Som Mondal <sommondal5872@gmail.com>
2025-04-29 22:44:12 +0530

    update

--- cure360/BACKEND/model/userModel.js
+++ cure360/BACKEND/model/userModel.js
@@ -1,42 +1,59 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+const notify = mongoose.Schema({
+    info:{
+        type:String
+    }
+},{timestamps:true})
 const userSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: true
+            required: true,
         },
         email: {
             type: String,
-            unique:true,
-            required: true
+            unique: true,
+            required: true,
+        },
+        phone: {
+            type: String,
+            unique: true,
+            required: true,
         },
         password: {
             type: String,
-            required: true
+            required: true,
         },
         role: {
             type: String,
             default: "patient",
-            enum :["hospital","doctor","patient"]
+            enum: ["admin", "hospital", "doctor", "patient"],
         },
-        
-        typeId:{
-            type: mongoose.Schema.ObjectId
+        dateOfBirth: {
+            type: Date,
+        },
+        verified: {
+            type: Boolean,
+            default: false,
+        },
+        notificationHistory:[notify],
+        typeId: {
+            type: mongoose.Schema.ObjectId,
         },
     },
     {
-        timestamps: true
+        timestamps: true,
     }
 );
-userSchema.pre("save", async function(next){
-    if(this.isModified("password")){
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next();
 });
-userSchema.methods.comparePassword = async function(pass){
+userSchema.methods.comparePassword = async function (pass) {
     return await bcrypt.compare(pass, this.password);
-}
+};
 export default mongoose.model("User", userSchema);
